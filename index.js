11b2156f4bb0144238971b1609164159d0bc3f91
yashnagla <naglayash@gmail.com>
2024-03-18 22:55:00 +0530

    Added Buttons

---
+++ index.js
@@ -0,0 +1,7 @@
+// ===============================================
+// Creating a portfolio tabbed component
+// ===============================================
+
+const p_btns = document.querySelector('.p-btns');
+const p_btn = document.querySelectorAll('p-btn');
+const p_img_elem = document.querySelectorAll('.img-overlay');
