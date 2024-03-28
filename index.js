0a86a9fa22636e9e06228be95910e92ddede0d60
yashnagla <nagla@gmail.com>
2024-03-28 20:29:31 +0530

    Updated

--- index.js
+++ index.js
@@ -5,3 +5,25 @@
 const p_btns = document.querySelector('.p-btns');
 const p_btn = document.querySelectorAll('p-btn');
 const p_img_elem = document.querySelectorAll('.img-overlay');
+
+p_btns.addEventListener( "click", function (e) {
+    const p_btn_clicked = e.target;
+    console.log(p_btn_clicked);
+
+    p_btn.forEach((curElem) => { curElem.classList.remove("p-btn-active"); });
+
+    p_btn_clicked.classList.add("p-btn-active");
+
+    // To find the number in data attribute
+    const btn_num = p_btn_clicked.dataset.btnNum;
+    console.log(btn_num);
+
+    const img_active = document.querySelectorAll(`.p-btn--${btn_num}`);
+
+    p_img_elem.forEach((curElem) => curElem.classList.add("p-image-not-active"));
+    
+    img_active.forEach((curElem) => curElem.classList.remove("p-image-not-active"));
+    
+    // portfolio-images
+    // p-btn--1
+});
