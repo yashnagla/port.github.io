74c0ec48686d23938e3a2aa09c39d1faed8c5a3d
yashnagla <nagla@gmail.com>
2024-03-29 01:26:25 +0530

    Updated

--- index.js
+++ index.js
@@ -2,28 +2,43 @@
 // Creating a portfolio tabbed component
 // ===============================================
 
-const p_btns = document.querySelector('.p-btns');
-const p_btn = document.querySelectorAll('p-btn');
-const p_img_elem = document.querySelectorAll('.img-overlay');
+const p_btns = document.querySelector(".p-btns");
+const p_btn = document.querySelectorAll("p-btn");
+const p_img_elem = document.querySelectorAll(".img-overlay");
 
-p_btns.addEventListener( "click", function (e) {
-    const p_btn_clicked = e.target;
-    console.log(p_btn_clicked);
+p_btns.addEventListener("click", function (e) {
+  const p_btn_clicked = e.target;
+  console.log(p_btn_clicked);
 
-    p_btn.forEach((curElem) => { curElem.classList.remove("p-btn-active"); });
+  p_btn.forEach((curElem) => {
+    curElem.classList.remove("p-btn-active");
+  });
 
-    p_btn_clicked.classList.add("p-btn-active");
+  p_btn_clicked.classList.add("p-btn-active");
 
-    // To find the number in data attribute
-    const btn_num = p_btn_clicked.dataset.btnNum;
-    console.log(btn_num);
+  // To find the number in data attribute
+  const btn_num = p_btn_clicked.dataset.btnNum;
+  console.log(btn_num);
 
-    const img_active = document.querySelectorAll(`.p-btn--${btn_num}`);
+  const img_active = document.querySelectorAll(`.p-btn--${btn_num}`);
 
-    p_img_elem.forEach((curElem) => curElem.classList.add("p-image-not-active"));
-    
-    img_active.forEach((curElem) => curElem.classList.remove("p-image-not-active"));
-    
-    // portfolio-images
-    // p-btn--1
+  p_img_elem.forEach((curElem) => curElem.classList.add("p-image-not-active"));
+
+  img_active.forEach((curElem) =>
+    curElem.classList.remove("p-image-not-active")
+  );
+});
+
+// Swiper JS Code
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  autoplay:{
+    delay:2500,
+    disableOnInteraction:false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
